9d2114f22cab71da758baa3639db7df4b84dd57f
daniel.nsfilho <daniel.nsfilho@SMP0582069W10-1>
2025-09-12 20:54:38 -0300

    Adicionar arquivos de projeto.

---
+++ TransformeseApp2.DTO/PessoaDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace TransformeseApp2.DTO
+{
+    // Classe com propriedades comuns
+    public abstract class PessoaDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+
+        // Método abstrato > Exemplo de abstração
+
+        public abstract void ExibirInfo();
+    }
+}
